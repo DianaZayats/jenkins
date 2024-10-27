6f2d73e7aeacb4214b86dc9c832ba0c059e8672e
Diana Zayats <marusya3651@gmail.com>
2024-10-27 20:30:42 +0200

    init

---
+++ test_app.py
@@ -0,0 +1,18 @@
+import unittest
+import app as tested_app
+import json
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = tested_app.app
+        self.client = self.app.test_client()
+
+    def test_get_hello_endpoint(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_data(as_text=True), 'Hello world from app! It is Pipeline testing.')
+
+if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
